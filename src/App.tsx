e24b2359e0114b1f41666af41bec9f25d50e2eb7
MohammedGitIt <mohammedunkown105@gmail.com>
2024-01-14 00:56:36 +0100

    adding assets and configuring the tailwind sorting styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-
-import React from 'react'
-
 function App() {
   return (
-    <div>App</div>
+    <div className="container flex justify-between gap-3  border border-gray-200 p-3 text-2xl font-bold text-slate-400 shadow-md ">
+      Hello people
+      <ul className="flex  flex-1 justify-between  text-blue-200 ">
+        <li>Homy</li>
+        <li>services</li>
+        <li>plans</li>
+      </ul>
+    </div>
   )
 }
 
